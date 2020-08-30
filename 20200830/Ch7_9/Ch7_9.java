0058b559ad618a58f2587367359910385ac344f5
xvpowerg <c911010083@gmail.com>
2020-08-30 16:56:26 +0800

    add anonymous implements interface

--- 20200830/Ch7_9/Ch7_9.java
+++ 20200830/Ch7_9/Ch7_9.java
@@ -4,11 +4,7 @@
  * and open the template in the editor.
  */
 package ch7_20200830.inner;
-
-/**
- *
- * @author xvpow
- */
+import java.util.function.Consumer;
 public class Ch7_9 {
 
     
@@ -21,18 +17,30 @@ public class Ch7_9 {
 	}
     }
     
+    static void printArrays(Consumer<Integer> c,int ... array){
+	for (int v : array){
+	    c.accept(v);
+	}
+    }
     public static void main(String[] args) {
 	Dog dog1 = new Dog();
 	超級好聲音(dog1);
 	Dog dog2 = new MeowDog();
 	超級好聲音(dog2);
-	//匿名內部類
+	//匿名內部類覆寫物件
 	Dog dog3 = new Dog(){
 	 public void bark(){
 	     System.out.println("支支!");
 	 }
 	};
 	超級好聲音(dog3);
+	
+	//匿名內部類實作介面
+	printArrays(new Consumer<Integer>(){
+	  public void accept(Integer v){
+	      System.out.print(v+" ");
+	  }
+	},6,8,9,10);
     }
     
 }
