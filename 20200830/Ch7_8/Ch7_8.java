0b7c3c2edf566a942d62b6f53ea622c354325a37
xvpowerg <c911010083@gmail.com>
2020-08-30 15:56:43 +0800

    Add files via upload

---
+++ 20200830/Ch7_8/Ch7_8.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch7_20200830.inner;
+
+/**
+ *
+ * @author xvpow
+ */
+public class Ch7_8 {
+
+    class TestInnerClass{
+	private int value;
+	public void setValue(int value){
+	    this.value = value;
+	}
+	public int getValue(){
+	    return value;
+	}
+    }
+    //用在非靜態方法中!!
+    //大多數情況
+    public void testClass(){
+	TestInnerClass tc = new TestInnerClass();
+	tc.setValue(100);
+	System.out.println(tc.getValue());
+    }
+    public static void main(String[] args) {
+	// TODO code application logic here
+	
+    }
+    
+}
