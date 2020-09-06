e7c620860973e26b16f31a2d3697bbfe289b0035
xvpowerg <c911010083@gmail.com>
2020-09-06 10:29:01 +0800

    Ch8_3

---
+++ 20200906/Ch8_3/Ch8_3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch8_20200906;
+
+/**
+ *
+ * @author xvpow
+ */
+public class Ch8_3 {
+
+    // Function Interface才可使用lambda
+    //Function Interface 介面當中抽象的方法只有一個
+    
+    static void testCopy(int[] data,CopyFile cp){
+	cp.copy(data);
+    }
+    public static void main(String[] args) {
+	
+	
+	
+    }
+    
+}
