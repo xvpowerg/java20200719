15b4d027ce52aa0a273e82a0f81587bfdaf5c059
xvpowerg <c911010083@gmail.com>
2020-09-06 17:02:41 +0800

    add 題2解答

--- 20200906/Ch8_12/Ch8_12.java
+++ 20200906/Ch8_12/Ch8_12.java
@@ -5,6 +5,7 @@
  */
 package ch8_20200906;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 public class Ch8_12 {
    static void test1(Predicate<Integer> p,String msg,int ...v){
 	boolean selected = false;
@@ -18,10 +19,25 @@ public class Ch8_12 {
 	   System.out.println(msg); 
 	}
     }   
+   
+   static String[] arrayToUp(UnaryOperator<String> uo,String ... v){
+       String[] tmpArray = new String[v.length];
+       for (int i =0;i<v.length;i++){
+	   tmpArray[i] = uo.apply(v[i]);
+       }
+      return tmpArray;
+   }
+   static String toUp(String st){
+       return st.toUpperCase();
+   }
     public static void main(String[] args) {
 	//題目1
 	test1(i-> i %2 == 0,"無偶數",8,9,10,2);
 	test1(i-> i %2 == 0,"無偶數",1,7,11,13);
+	//題目2
+	String[] upArray =  arrayToUp(Ch8_12::toUp,"ken","vivin","lindy");
+	System.out.println(upArray[0]);
+	System.out.println(upArray[1]);
     }
     
 }
