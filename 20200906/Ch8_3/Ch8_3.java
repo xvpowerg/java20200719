48692dbdca491da58b6b07aad6e19e13463b1f5b
xvpowerg <c911010083@gmail.com>
2020-09-06 10:37:20 +0800

    add lambda test

--- 20200906/Ch8_3/Ch8_3.java
+++ 20200906/Ch8_3/Ch8_3.java
@@ -18,8 +18,27 @@ public class Ch8_3 {
 	cp.copy(data);
     }
     public static void main(String[] args) {
+	//Function Interface 的方法參數只有一組且無回傳值
+	   //標準寫法
+	   int[] data = {56,30,92};
+	   CopyFile c1 = (int[] a)->{	       
+	       System.out.println("C1:"+a[0]);
+	   };	   
+	   testCopy(data,c1);
+	   //簡寫1
+	   //簡寫2
+	   //簡寫3
+	   
 	
+	//Function Interface 的方法參數只有兩組且無回傳值
 	
+	//Function Interface 的方法參數只有一組且有回傳值
+	
+	//Function Interface 的方法參數只有兩組且有回傳值
+	
+	//Function Interface 的無方法參數且有回傳值
+	
+	//Function Interface 的無方法參數且無回傳值
 	
     }
     
