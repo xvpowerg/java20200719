0cbbbfb2122cabac724b0302167ef5740df4337c
xvpowerg <c911010083@gmail.com>
2020-09-13 14:38:46 +0800

    Update Ch9_7.java

--- 20200913/Ch9_7/Ch9_7.java
+++ 20200913/Ch9_7/Ch9_7.java
@@ -11,16 +11,31 @@ public class Ch9_7 {
     public static void main(String[] args) {
 	//Set 結構
 	//Set 內容不可重複　無法使用index取值
-	//Set 速度比List快 
-	HashSet<Integer> set = new HashSet<>();
+	//Set 查詢速度比List好
+	/*HashSet<Integer> set = new HashSet<>();
 	set.add(95);
 	set.add(15);
 	set.add(700);
 	set.add(15);
 	set.add(81);
 	set.add(62);
-	set.forEach(System.out::println);
+	set.forEach(System.out::println);*/
 	
+	Student st1 = new Student("Ken",20);
+	Student st2 = new Student("Vivin",70);
+	Student st3 = new Student("Lindy",84);
+	Student st4 = new Student("Vivin",70);
+	
+	//HashSet 判定是否相等順序是
+	//hashCode
+	//equals
+	HashSet<Student> set2 = new HashSet<>();
+	set2.add(st1);
+	set2.add(st2);
+	set2.add(st3);
+	set2.add(st4);
+	
+	set2.forEach(System.out::println);
     }
     
 }
