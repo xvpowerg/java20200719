95a0f74d9f1c7d97ab9565557f1b883e68811e9a
xvpowerg <c911010083@gmail.com>
2020-09-20 11:11:00 +0800

    Ch10_3

---
+++ 20200920/Ch10_3/Ch10_3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch10_20200920;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author xvpow
+ */
+public class Ch10_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	Map<Integer,String> hashMap = new HashMap();
+	hashMap.put(10, "A1");
+	hashMap.put(15, "B2");
+	hashMap.put(76, "C3");
+	hashMap.put(19, "D4");
+	//Key 不存在
+	// compute 當Key不存在 會執行BiFunction 但是v2 為null
+	//會將BiFunction的return 寫回Map
+	hashMap.compute(17, (v1,v2)->{
+	    System.out.println("compute Key不存在:"+v1+":"+v2);
+	    return "X5";}
+	);
+	System.out.println(hashMap);//{17=X5, 19=D4, 10=A1, 76=C3, 15=B2}
+	//hashMap.computeIfAbsent(Integer.SIZE, mappingFunction)
+	//hashMap.computeIfPresent(Integer.SIZE, remappingFunction)
+    }
+    
+}
---
+++ 20200920/Ch10_3/README.md
@@ -0,0 +1,28 @@
+# Map 的compute,computeIfAbsent,computeIfPresent方法討論
+## Map宣告如下
+```java
+	Map<Integer,String> hashMap = new HashMap();
+	hashMap.put(10, "A1");
+	hashMap.put(15, "B2");
+	hashMap.put(76, "C3");
+	hashMap.put(19, "D4");
+```
+## 假設Key 不存在
+```java
+	Map<Integer,String> hashMap = new HashMap();
+	hashMap.put(10, "A1");
+	hashMap.put(15, "B2");
+	hashMap.put(76, "C3");
+	hashMap.put(19, "D4");
+```
+## compute 當Key不存在 
+* 會執行BiFunction 但是v2 為null
+* 會將BiFunction的return 寫回Map
+```java
+	hashMap.compute(17, (v1,v2)->{
+	    System.out.println("compute Key不存在:"+v1+":"+v2);
+	    return "X5";}
+	);
+	System.out.println(hashMap);//{17=X5, 19=D4, 10=A1, 76=C3, 15=B2}
+```
+
