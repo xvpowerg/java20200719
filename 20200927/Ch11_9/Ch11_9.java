60df1dd1e90f85d3039d77521484301a0c626303
xvpowerg <c911010083@gmail.com>
2020-09-27 16:16:58 +0800

    Update Ch11_9.java

--- 20200927/Ch11_9/Ch11_9.java
+++ 20200927/Ch11_9/Ch11_9.java
@@ -5,23 +5,17 @@
  */
 package ch11_20200927;
 import java.util.Optional;
-/**
- *
- * @author xvpow
- */
 public class Ch11_9 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 //	Teacher t1 = new Teacher(null,20);
 //	if ( t1.getName().length() < 0 || t1.getName().length() > 10) {
 //	    System.out.println("錯誤的姓名!");
 //	}
-// Optional用台統一化避免null問題
+// Optional用來統一化避免null問題的方法
   Optional<String> nameOption =   Optional.empty();
       // System.out.println(nameOption.get());
+      //nameOption = Optional.of(null);//不可放置null
+      nameOption = Optional.ofNullable(null);//可放置null
     if (nameOption.isPresent()){
 	System.out.println(nameOption.get());
     }
