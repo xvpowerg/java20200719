6cacc51c8da94f12ef6bedbde1bf80959b153020
xvpowerg <c911010083@gmail.com>
2020-10-04 16:24:14 +0800

    Add files via upload

--- 20201004/ch12_10/Ch12_10.java
+++ 20201004/ch12_10/Ch12_10.java
@@ -11,7 +11,7 @@ public class Ch12_10 {
     
     
     public static void main(String[] args) throws Exception{
-	    
+	    //要序列化的物件 必須實作Serializable
 	Student st1 = new Student("Ken",96);
 	File file = new File("C:\\MyDir\\student.obj");
 	try(FileOutputStream fout = new FileOutputStream(file);
