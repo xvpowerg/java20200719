e1fadbc0e79b53f7017b804366f5d12416a32b4c
xvpowerg <c911010083@gmail.com>
2020-10-04 15:19:01 +0800

    Update Ch12_7.java

--- 20201004/ch12_7/Ch12_7.java
+++ 20201004/ch12_7/Ch12_7.java
@@ -18,12 +18,30 @@ public class Ch12_7 {
 	    /*Body...
 	    name:Test2
 	    name:Test1*/
+//	try(TestClose t1 = new TestClose("Test1",false);
+//	TestClose t2 = new TestClose("Test2",false);){
+//	    System.out.println("Body...");	   	    
+//	}catch(Exception ex){
+//	    System.out.println("Exception:"+ex);	   
+//	}
+    
+	    /*
+	如果Body拋出例外的順序
+	Body...
+	name:Test2
+	name:Test1
+	Exception:java.lang.Exception: Body...ex
+	
+	*/
 	try(TestClose t1 = new TestClose("Test1",false);
 	TestClose t2 = new TestClose("Test2",false);){
-	    System.out.println("Body...");	   	    
+	    System.out.println("Body...");	
+	   throw new Exception("Body...ex");
 	}catch(Exception ex){
 	    System.out.println("Exception:"+ex);	   
 	}
+	
+	
     }
     
 }
