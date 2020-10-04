b54e6504eff87379ee2334d47403a7ce95310da3
xvpowerg <c911010083@gmail.com>
2020-10-04 16:17:09 +0800

    Ch12_10

---
+++ 20201004/ch12_10/Ch12_10.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch12_20201004;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+public class Ch12_10 {
+    
+    
+    public static void main(String[] args) throws Exception{
+	    
+	Student st1 = new Student("Ken",96);
+	File file = new File("C:\\MyDir\\student.obj");
+	try(FileOutputStream fout = new FileOutputStream(file);
+	   ObjectOutputStream ooput = new ObjectOutputStream(fout);){
+	    ooput.writeObject(st1);
+	}
+    }
+    
+}
